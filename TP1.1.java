7ef0098510dabcd828a27c232cd3e4a851880165
s0lyang <147157676+s0lyang@users.noreply.github.com>
2024-03-25 15:55:42 -0300

    Add files via upload

---
+++ TP1.1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    
+    Scanner sc=new Scanner (System.in);
+
+    int num;
+
+    System.out.print("Ingrese un número: ");
+    num=sc.nextInt();
+
+    System.ou.print("El número es: "s+num);
+}
---
+++ Tp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Tp {
+
+    int edad;
+
+    System.out.print("Ingrese su edad: ");
+    edad=sc.nextInt();
+    
+    System.out.print("La edad que tendrá el año que viene, será: "+(edad+1));
+}
